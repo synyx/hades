3275e2081cb4610a779b82153d6c2378faa0b149
Oliver Gierke <info@olivergierke.de>
2008-02-08 13:27:20 +0000

    * use User entity class as example instead of Person
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/spring-jpa-dao/trunk@261 5a64d73e-33d6-4ccc-9058-23f8668ecac9

---
+++ src/test/java/org/synyx/jpa/support/test/domain/Role.java
@@ -0,0 +1,63 @@
+package org.synyx.jpa.support.test.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+/**
+ * @author Oliver Gierke
+ */
+@Entity
+public class Role implements org.synyx.jpa.support.Entity<Integer> {
+
+    private static final String PREFIX = "ROLE_";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+
+    /**
+     * 
+     */
+    public Role() {
+
+        // TODO Auto-generated constructor stub
+    }
+
+
+    /**
+     * @param name
+     */
+    public Role(String name) {
+
+        this.name = name;
+    }
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.synyx.jpa.support.Entity#getId()
+     */
+    public Integer getId() {
+
+        return id;
+    }
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+
+        return PREFIX + name;
+    }
+}
