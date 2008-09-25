d36263c17e001e4fd1c3c8048d8740081bf13776
Oliver Gierke <info@olivergierke.de>
2008-09-25 08:14:01 +0000

    * added first draft of {{{EclipseLink}}} DAO implementation
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@2767 5a64d73e-33d6-4ccc-9058-23f8668ecac9

---
+++ src/test/java/org/synyx/hades/dao/test/EclipseLinkIntegrationTest.java
@@ -0,0 +1,21 @@
+package org.synyx.hades.dao.test;
+
+import org.junit.Test;
+
+
+/**
+ * Integration test for EclipseLink implementation of {@code ExtendedGenericDao}
+ * . Not implemented to be run yet, due to configuration problems. I don't want
+ * to break the test suite right now.
+ * 
+ * @author Oliver Gierke - gierke@synyx.de
+ */
+// @ContextConfiguration(locations = { "classpath:applicationContext.xml",
+// "classpath:eclipselink.xml" }, inheritLocations = false)
+public class EclipseLinkIntegrationTest {
+
+    @Test
+    public void testFoo() {
+
+    }
+}
