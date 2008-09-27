74ef60ee60e085c3abcbf88eba2f839d9e99e91d
Oliver Gierke <info@olivergierke.de>
2008-09-27 16:08:50 +0000

    * JavaDoc fixes
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades@2795 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- trunk/src/main/java/org/synyx/hades/domain/support/AbstractAuditable.java
+++ trunk/src/main/java/org/synyx/hades/domain/support/AbstractAuditable.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2002-2008 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.synyx.hades.domain.support;
@@ -29,7 +29,8 @@ import org.synyx.hades.domain.Persistable;
 
 
 /**
- * Abstract base class for auditable entities. Stores the audition values in persistent fields
+ * Abstract base class for auditable entities. Stores the audition values in
+ * persistent fields.
  * 
  * @author Oliver Gierke - gierke@synyx.de
  * @param <U> the auditing type. Typically some kind of user.
@@ -68,7 +69,9 @@ public abstract class AbstractAuditable<U extends Persistable<PK>, PK extends Se
     /*
      * (non-Javadoc)
      * 
-     * @see org.synyx.hades.hades.domain.Auditable#setCreatedBy(org.synyx.hades.hades.domain.Identifyable)
+     * @see
+     * org.synyx.hades.hades.domain.Auditable#setCreatedBy(org.synyx.hades.hades
+     * .domain.Identifyable)
      */
     public void setCreatedBy(final U createdBy) {
 
@@ -112,7 +115,9 @@ public abstract class AbstractAuditable<U extends Persistable<PK>, PK extends Se
     /*
      * (non-Javadoc)
      * 
-     * @see org.synyx.hades.hades.domain.Auditable#setLastModifiedBy(org.synyx.hades.hades.domain.Identifyable)
+     * @see
+     * org.synyx.hades.hades.domain.Auditable#setLastModifiedBy(org.synyx.hades
+     * .hades.domain.Identifyable)
      */
     public void setLastModifiedBy(final U lastModifiedBy) {
 
@@ -134,7 +139,8 @@ public abstract class AbstractAuditable<U extends Persistable<PK>, PK extends Se
     /*
      * (non-Javadoc)
      * 
-     * @see org.synyx.hades.hades.domain.Auditable#setLastModified(java.util.Date)
+     * @see
+     * org.synyx.hades.hades.domain.Auditable#setLastModified(java.util.Date)
      */
     public void setLastModified(final Date lastModifiedDate) {
 
