0b7d89c30553f36dcf4ab732ab8fe232caed90bd
Oliver Gierke <info@olivergierke.de>
2008-10-18 11:57:24 +0000

    * fixed comments
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@3087 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- src/test/java/org/synyx/hades/domain/AuditableUser.java
+++ src/test/java/org/synyx/hades/domain/AuditableUser.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2002-2008 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.synyx.hades.domain;
@@ -23,9 +23,9 @@ import org.synyx.hades.domain.support.AbstractAuditable;
 
 
 /**
- * Sample auditable user to demonstrate working with
- * {@code AbstractAuditableEntity}. No declaration of an ID is necessary.
- * Furthermore alle auditioning information has to be declared explicitly.
+ * Sample auditable user to demonstrate working with {@code
+ * AbstractAuditableEntity}. No declaration of an ID is necessary. Furthermore
+ * no auditing information has to be declared explicitly.
  * 
  * @author Oliver Gierke - gierke@synyx.de
  */
