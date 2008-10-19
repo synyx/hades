3b2a08d87663cf572a42cb7a2e36b7a5e2643687
Oliver Gierke <info@olivergierke.de>
2008-10-19 12:53:32 +0000

    * implemented {{{equals(Object obj)}}} as well as {{{hashCode()}}} for {{{AbstractPersistable}}}  * provided test case for implementation  * fixed #24
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades@3093 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- trunk/src/main/java/org/synyx/hades/domain/AbstractPersistable.java
+++ trunk/src/main/java/org/synyx/hades/domain/AbstractPersistable.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2002-2008 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.synyx.hades.domain;
@@ -85,5 +85,43 @@ public abstract class AbstractPersistable<PK extends Serializable> implements
 
         return "Entity of type " + this.getClass().getName() + " with id: "
                 + getId();
+    }
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (!obj.getClass().equals(this.getClass())) {
+            return false;
+        }
+
+        AbstractPersistable<?> that = (AbstractPersistable<?>) obj;
+
+        return null == this.getId() ? false : this.getId().equals(that.getId());
+    }
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+
+        int hashCode = 17;
+
+        hashCode += null == getId() ? 0 : getId().hashCode() * 31;
+
+        return hashCode;
     }
 }
