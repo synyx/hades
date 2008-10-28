c61099d227fff53eac96144a8ce2cd539748844b
Oliver Gierke <info@olivergierke.de>
2008-10-28 12:12:33 +0000

    * fixed # 30: Polish toString methods by using String.format() - http://trac.synyx.org/hades/ticket/30  * use String.format() on a few other places, too
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@3168 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- src/main/java/org/synyx/hades/domain/page/PageImpl.java
+++ src/main/java/org/synyx/hades/domain/page/PageImpl.java
@@ -162,6 +162,7 @@ public class PageImpl<T> implements Page<T> {
             contentType = content.get(0).getClass().getName();
         }
 
-        return "Page " + number + " of " + getTotalPages() + " " + contentType;
+        return String.format("Page %s of %d %s", number, getTotalPages(),
+                contentType);
     }
 }
