ff27206006bc82c125a0fc21b9da750662febf31
Oliver Gierke <info@olivergierke.de>
2008-11-26 17:35:24 +0000

    * polished !JavaDoc
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@3550 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- src/main/java/org/synyx/hades/domain/page/PageRequest.java
+++ src/main/java/org/synyx/hades/domain/page/PageRequest.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2002-2008 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.synyx.hades.domain.page;
@@ -28,7 +28,8 @@ public class PageRequest implements Pageable {
 
 
     /**
-     * Creates a new {@code PageRequest}.
+     * Creates a new {@code PageRequest}. Pages are zero indexed, thus providing
+     * 0 for {@code page} will return the first page.
      * 
      * @param size
      * @param page
