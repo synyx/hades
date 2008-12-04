67149bb1afb31abb8c4f6ca74ef9838642bcd8e4
Oliver Gierke <info@olivergierke.de>
2008-12-04 15:05:57 +0000

    * removed obsolete type parameter from {{{AuditorAware}}} (no need to know the id type)
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades@3645 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- trunk/src/main/java/org/synyx/hades/domain/AuditorAware.java
+++ trunk/src/main/java/org/synyx/hades/domain/AuditorAware.java
@@ -1,24 +1,20 @@
 /*
  * Copyright 2002-2008 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.synyx.hades.domain;
-
-import java.io.Serializable;
-
-
 
 /**
  * Interface for components that are aware of the application's current auditor.
@@ -26,9 +22,8 @@ import java.io.Serializable;
  * 
  * @author Oliver Gierke - gierke@synyx.de
  * @param <T> the type of the auditing instance
- * @param <PK> the type of the auditing instance's identifier
  */
-public interface AuditorAware<T extends Persistable<PK>, PK extends Serializable> {
+public interface AuditorAware<T extends Persistable<?>> {
 
     /**
      * Returns the current auditor of the application.
