292ed7e7171b5e7604df3b0cd242e96f0805cf4c
Oliver Gierke <info@olivergierke.de>
2009-03-20 12:34:12 +0000

    * added utility package as well as a class to determine domain class from generic DAO interface declaration
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@4998 5a64d73e-33d6-4ccc-9058-23f8668ecac9

---
+++ hades/src/main/java/org/synyx/hades/util/ClassUtils.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.synyx.hades.util;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
+
+import org.synyx.hades.dao.ExtendedGenericDao;
+import org.synyx.hades.dao.GenericDao;
+import org.synyx.hades.domain.Persistable;
+
+
+/**
+ * Utility class to work with classes.
+ * 
+ * @author Oliver Gierke - gierke@synyx.de
+ */
+public abstract class ClassUtils {
+
+    /**
+     * Hades own DAO interfaces.
+     */
+    public static final List<String> HADES_DAO_INTERFACE_NAMES =
+            Arrays.asList(GenericDao.class.getName(), ExtendedGenericDao.class
+                    .getName());
+
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private ClassUtils() {
+
+    }
+
+
+    /**
+     * Returns the domain class the given class is declared for. Will introspect
+     * the given class for extensions of {@link GenericDao} or
+     * {@link ExtendedGenericDao} and retrieve the {@link Persistable} type from
+     * its generics declaration.
+     * 
+     * @param clazz
+     * @return the domain class the given class is DAO for or {@code null} if
+     *         none found.
+     */
+    @SuppressWarnings("unchecked")
+    public static Class<? extends Persistable<?>> getDomainClass(Class<?> clazz) {
+
+        for (Type type : clazz.getGenericInterfaces()) {
+
+            if (type instanceof ParameterizedType) {
+
+                ParameterizedType parammeterizedType = (ParameterizedType) type;
+
+                if (isGenericDao(parammeterizedType)) {
+
+                    return (Class<? extends Persistable<?>>) parammeterizedType
+                            .getActualTypeArguments()[0];
+                }
+            }
+
+            Class<? extends Persistable<?>> result =
+                    getDomainClass((Class<?>) type);
+
+            if (null != result) {
+                return result;
+            }
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Returns whether a {@link ParameterizedType} is a {@link GenericDao} or
+     * {@link ExtendedGenericDao}.
+     * 
+     * @param type
+     * @return
+     */
+    private static boolean isGenericDao(ParameterizedType type) {
+
+        boolean isExtendedGenericDao =
+                type.getRawType().equals(ExtendedGenericDao.class);
+        boolean isGenericDao = type.getRawType().equals(GenericDao.class);
+
+        return isGenericDao || isExtendedGenericDao;
+    }
+}
