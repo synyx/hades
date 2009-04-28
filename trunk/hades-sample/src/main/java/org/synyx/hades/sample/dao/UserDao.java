c334a4c2f76a68b4eca0ebd5550b4191a1b3f500
Oliver Gierke <info@olivergierke.de>
2009-04-28 19:43:57 +0000

    * extended DAO methods to reflect all ways of creating a query from a method * exteded JavaDoc to document this feature
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades@5465 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- trunk/hades-sample/src/main/java/org/synyx/hades/sample/dao/UserDao.java
+++ trunk/hades-sample/src/main/java/org/synyx/hades/sample/dao/UserDao.java
@@ -3,6 +3,7 @@ package org.synyx.hades.sample.dao;
 import java.util.List;
 
 import org.synyx.hades.dao.GenericDao;
+import org.synyx.hades.dao.Query;
 import org.synyx.hades.sample.domain.User;
 
 
@@ -16,10 +17,35 @@ import org.synyx.hades.sample.domain.User;
 public interface UserDao extends GenericDao<User, Long>, UserDaoCustom {
 
     /**
-     * Find all users with the given lastname.
+     * Find the user with the given username. This method will be translated
+     * into a query using the {@link javax.persistence.NamedQuery} annotation at
+     * the {@link User} class.
      * 
      * @param lastname
      * @return
      */
-    List<User> findByUsername(String username);
+    User findByTheUsersName(String username);
+
+
+    /**
+     * Find all users with the given lastname. This method will be translated
+     * into a query by constructing it directly from the method name as there is
+     * no other query declared.
+     * 
+     * @param lastname
+     * @return
+     */
+    List<User> findByLastname(String lastname);
+
+
+    /**
+     * Returns all users with the given firstname. This method will be
+     * translated into a query using the one declared in the {@link Query}
+     * annotation declared one.
+     * 
+     * @param firstname
+     * @return
+     */
+    @Query("from User u where u.firstname = ?")
+    List<User> findByFirstname(String firstname);
 }
