3afe6aba7cbe01ab76853d0cb9a27f449c477bd8
Oliver Gierke <info@olivergierke.de>
2009-04-29 05:40:03 +0000

    * extended User class to also carry firstname and lastname
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@5471 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- hades-sample/src/main/java/org/synyx/hades/sample/domain/User.java
+++ hades-sample/src/main/java/org/synyx/hades/sample/domain/User.java
@@ -1,5 +1,6 @@
 package org.synyx.hades.sample.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.NamedQuery;
 
@@ -12,14 +13,21 @@ import org.synyx.hades.domain.support.AbstractPersistable;
  * @author Oliver Gierke - gierke@synyx.de
  */
 @Entity
-@NamedQuery(name = "User.findByUserblame", query = "from User u where u.username = ?")
+@NamedQuery(name = "User.findByTheUsersName", query = "from User u where u.username = ?")
 public class User extends AbstractPersistable<Long> {
 
     private static final long serialVersionUID = -2952735933715107252L;
 
+    @Column(unique = true)
     private String username;
 
+    private String firstname;
+    private String lastname;
 
+
+    /**
+     * Creates a new user instance.
+     */
     public User() {
 
     }
@@ -42,5 +50,41 @@ public class User extends AbstractPersistable<Long> {
     public void setUsername(String username) {
 
         this.username = username;
+    }
+
+
+    /**
+     * @return the firstname
+     */
+    public String getFirstname() {
+
+        return firstname;
+    }
+
+
+    /**
+     * @param firstname the firstname to set
+     */
+    public void setFirstname(String firstname) {
+
+        this.firstname = firstname;
+    }
+
+
+    /**
+     * @return the lastname
+     */
+    public String getLastname() {
+
+        return lastname;
+    }
+
+
+    /**
+     * @param lastname the lastname to set
+     */
+    public void setLastname(String lastname) {
+
+        this.lastname = lastname;
     }
 }
