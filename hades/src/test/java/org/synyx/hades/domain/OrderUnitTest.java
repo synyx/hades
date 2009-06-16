0b35ffdead05761023b53204d8c08914fe9a27de
Oliver Gierke <info@olivergierke.de>
2009-06-16 08:45:34 +0000

    * added unit test for @Order@
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@5957 5a64d73e-33d6-4ccc-9058-23f8668ecac9

---
+++ hades/src/test/java/org/synyx/hades/domain/OrderUnitTest.java
@@ -0,0 +1,28 @@
+package org.synyx.hades.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for {@link Order}.
+ * 
+ * @author Oliver Gierke - gierke@synyx.de
+ */
+public class OrderUnitTest {
+
+    @Test
+    public void jpaValueMapping() throws Exception {
+
+        assertEquals(Order.ASCENDING, Order.fromJpaValue("asc"));
+        assertEquals(Order.DESCENDING, Order.fromJpaValue("desc"));
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void rejectsInvalidString() throws Exception {
+
+        Order.fromJpaValue("foo");
+    }
+}
