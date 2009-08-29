18965e772644549bac234ce49619b308ed8e2f6c
Oliver Gierke <info@olivergierke.de>
2009-08-29 09:21:42 +0000

    * added @Modifying@ annotation (should have been included in r6728)
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@6730 5a64d73e-33d6-4ccc-9058-23f8668ecac9

---
+++ hades/src/main/java/org/synyx/hades/dao/Modifying.java
@@ -0,0 +1,18 @@
+package org.synyx.hades.dao;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Indicates a method should be regarded as modifying query.
+ * 
+ * @author Oliver Gierke - gierke@synyx.de
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Modifying {
+
+}
