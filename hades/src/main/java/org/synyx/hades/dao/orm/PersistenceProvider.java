ecc553936954164cd8f53209c431ad93f0382550
Oliver Gierke <info@olivergierke.de>
2009-11-11 10:13:25 +0000

    * added @QueryExtractor@ interface as well as enum-based implementation to use the underlying persistence providers interface
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@7853 5a64d73e-33d6-4ccc-9058-23f8668ecac9

---
+++ hades/src/main/java/org/synyx/hades/dao/orm/PersistenceProvider.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.synyx.hades.dao.orm;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.eclipse.persistence.jpa.JpaQuery;
+import org.hibernate.ejb.HibernateQuery;
+import org.synyx.hades.dao.query.QueryExtractor;
+import org.synyx.hades.util.ClassUtils;
+
+
+/**
+ * Enumeration representing peristence providers to be used with Hades.
+ * 
+ * @author Oliver Gierke - gierke@synyx.de
+ */
+public enum PersistenceProvider implements QueryExtractor {
+
+    /**
+     * Hibernate persistence provider.
+     */
+    HIBERNATE(GenericHibernateJpaDao.class,
+            "org.hibernate.ejb.HibernateEntityManager") {
+
+        public String extractQueryString(Query query) {
+
+            return ((HibernateQuery) query).getHibernateQuery()
+                    .getQueryString();
+        }
+    },
+
+    /**
+     * EclipseLink persistence provider.
+     */
+    ECLIPSELINK(GenericEclipseLinkJpaDao.class,
+            "org.eclipse.persistence.jpa.JpaEntityManager") {
+
+        public String extractQueryString(Query query) {
+
+            return ((JpaQuery) query).getDatabaseQuery().getJPQLString();
+        }
+    },
+
+    /**
+     * 
+     */
+    GENERIC_JPA(GenericJpaDao.class, "javax.persistence.EntityManager") {
+
+        public String extractQueryString(Query query) {
+
+            return null;
+        }
+    };
+
+    @SuppressWarnings("unchecked")
+    private Class<? extends GenericJpaDao> daoBaseClass;
+    private String entityManagerClassName;
+
+
+    /**
+     * Creates a new {@link PersistenceProvider}.
+     * 
+     * @param daoBaseClass the DAO base class to be used with the persistence
+     *            provider
+     * @param entityManagerClassName the name of the provider specific
+     *            {@link EntityManager} implementation
+     */
+    @SuppressWarnings("unchecked")
+    private PersistenceProvider(Class<? extends GenericJpaDao> daoBaseClass,
+            String entityManagerClassName) {
+
+        this.daoBaseClass = daoBaseClass;
+        this.entityManagerClassName = entityManagerClassName;
+    }
+
+
+    /**
+     * Returns the DAO base class to be used.
+     * 
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public Class<? extends GenericJpaDao> getDaoBaseClass() {
+
+        return daoBaseClass;
+    }
+
+
+    /**
+     * Determines the {@link PersistenceProvider} from the given
+     * {@link EntityManager}. If no special one can be determined
+     * {@value #GENERIC_JPA} will be returned.
+     * 
+     * @param em
+     * @return
+     */
+    public static PersistenceProvider fromEntityManager(EntityManager em) {
+
+        for (PersistenceProvider provider : values()) {
+            if (ClassUtils.isEntityManagerOfType(em,
+                    provider.entityManagerClassName)) {
+                return provider;
+            }
+        }
+
+        return GENERIC_JPA;
+    }
+}
---
+++ hades/src/main/java/org/synyx/hades/dao/query/QueryExtractor.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2002-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.synyx.hades.dao.query;
+
+import javax.persistence.Query;
+
+
+/**
+ * Interface to hide different implementations to extract the original JPA query
+ * string from a {@link Query}.
+ * 
+ * @author Oliver Gierke - gierke@synyx.de
+ */
+public interface QueryExtractor {
+
+    /**
+     * Reverse engineers the query string from the {@link Query} object. This
+     * requires provider specific API as JPA does not provide access to the
+     * underlying query string as soon as one has created a {@link Query}
+     * instance of it.
+     * 
+     * @param query
+     * @return the query string representing the query or {@literal null} if
+     *         resolving is not possible.
+     */
+    String extractQueryString(Query query);
+}
