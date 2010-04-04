1cd0fd6fb0c17915762e814d3c0b8522745e1f4e
Oliver Gierke <info@olivergierke.de>
2010-04-04 09:56:35 +0000

    * fixed years in licence texts
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@10229 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- hades/src/test/java/org/synyx/hades/domain/auditing/AuditingAdviceIntegrationTest.java
+++ hades/src/test/java/org/synyx/hades/domain/auditing/AuditingAdviceIntegrationTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.synyx.hades.domain.auditing;
 
 import static org.junit.Assert.*;
@@ -13,7 +28,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.synyx.hades.dao.AuditableUserDao;
 import org.synyx.hades.dao.GenericDao;
-import org.synyx.hades.domain.auditing.AuditingAdvice;
 
 
 /**
@@ -57,8 +71,8 @@ public class AuditingAdviceIntegrationTest {
 
 
     /**
-     * Checks that the advice intercepts calls to
-     * {@link GenericDao#save(List)} correctly.
+     * Checks that the advice intercepts calls to {@link GenericDao#save(List)}
+     * correctly.
      */
     @Test
     public void touchesManyEntitiesOnSave() {
