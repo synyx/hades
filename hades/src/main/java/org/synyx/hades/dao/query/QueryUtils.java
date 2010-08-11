a8818f183022d8f4a0c1b5a2a95bcc8858eafeb0
Oliver Gierke <info@olivergierke.de>
2010-08-11 16:12:40 +0000

    * ported fix for #352 to Hades 2.x branch * references #352
    git-svn-id: svn+ssh://svn.synyx.de/var/svn/synyx/opensource/hades/trunk@12492 5a64d73e-33d6-4ccc-9058-23f8668ecac9

--- hades/src/main/java/org/synyx/hades/dao/query/QueryUtils.java
+++ hades/src/main/java/org/synyx/hades/dao/query/QueryUtils.java
@@ -44,9 +44,9 @@ public abstract class QueryUtils {
     public static final String READ_ALL_QUERY = "select x from %s x";
     private static final String DEFAULT_ALIAS = "x";
 
-    private static final Pattern COUNT_MATCH =
-            compile("(select\\s*((distinct )?.*)\\s*)?(from\\s*\\w*\\s*)(\\w*)(.* where.*)?",
-                    CASE_INSENSITIVE);
+    private static final Pattern COUNT_MATCH = compile(
+            "(select\\s*((distinct )?.*)\\s*)?(from\\s*\\w*\\s*)(\\w*)(.*)",
+            CASE_INSENSITIVE);
     private static final String COUNT_REPLACEMENT = "select count($3$5) $4$5$6";
 
     private static final Pattern ALIAS_MATCH;
