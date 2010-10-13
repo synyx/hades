6ec66356372b93be98e930ab827edd1b876ad668
Oliver Gierke <info@olivergierke.de>
2010-10-13 17:14:33 +0200

    Use @*@ as count directive as OR-Mappers (i.e. Hibernate) will create invalid query otherwise.

--- hades/src/main/java/org/synyx/hades/dao/query/QueryUtils.java
+++ hades/src/main/java/org/synyx/hades/dao/query/QueryUtils.java
@@ -38,7 +38,7 @@ import org.synyx.hades.util.ClassUtils;
  */
 public abstract class QueryUtils {
 
-    public static final String COUNT_QUERY_STRING = "select count(x) from %s x";
+    public static final String COUNT_QUERY_STRING = "select count(*) from %s x";
 
     public static final String DELETE_ALL_QUERY_STRING = "delete from %s x";
     public static final String READ_ALL_QUERY = "select x from %s x";
